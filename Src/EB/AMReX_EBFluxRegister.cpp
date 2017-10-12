4b2f491afa3cd33640ec94d352819cc0dd24248d
Marc Day <MSDay@lbl.gov>
2017-10-11 18:10:11 -0700

    Add signature for eb fr ops that assumes fluxes already scaled by apreatures.

--- Src/EB/AMReX_EBFluxRegister.cpp
+++ Src/EB/AMReX_EBFluxRegister.cpp
@@ -89,6 +89,35 @@ EBFluxRegister::CrseAdd (const MFIter& mfi,
 
 
 void
+EBFluxRegister::CrseAdd (const MFIter& mfi,
+                         const std::array<FArrayBox const*, AMREX_SPACEDIM>& flux,
+                         const Real* dx, Real dt,
+                         const FArrayBox& volfrac)
+{
+    BL_ASSERT(m_crse_data.nComp() == flux[0]->nComp());
+
+    if (m_crse_fab_flag[mfi.LocalIndex()] == crse_cell) {
+        return;  // this coarse fab is not close to fine fabs.
+    }
+
+    FArrayBox& fab = m_crse_data[mfi];
+    const Box& bx = mfi.tilebox();
+    const int nc = fab.nComp();
+
+    const IArrayBox& amrflag = m_crse_flag[mfi];
+
+    amrex_eb_flux_reg_crseadd_v(BL_TO_FORTRAN_BOX(bx),
+                                BL_TO_FORTRAN_ANYD(fab),
+                                BL_TO_FORTRAN_ANYD(amrflag),
+                                AMREX_D_DECL(BL_TO_FORTRAN_ANYD(*flux[0]),
+                                             BL_TO_FORTRAN_ANYD(*flux[1]),
+                                             BL_TO_FORTRAN_ANYD(*flux[2])),
+                                BL_TO_FORTRAN_ANYD(volfrac),
+                                dx, &dt,&nc);
+}
+
+
+void
 EBFluxRegister::FineAdd (const MFIter& mfi,
                          const std::array<FArrayBox const*, AMREX_SPACEDIM>& flux,
                          const Real* dx, Real dt,
@@ -151,6 +180,91 @@ EBFluxRegister::FineAdd (const MFIter& mfi,
                                                               BL_TO_FORTRAN_ANYD(*areafrac[2])),
                                                  dx, &dt, &nc, &idim, &side,
                                                  m_ratio.getVect());
+                }
+            }
+        }
+    }
+
+    FArrayBox dmgrow(amrex::grow(tbx,m_ratio),nc);
+    dmgrow.setVal(0.0);
+    const Box& tbxg1 = amrex::grow(tbx,1);
+    dmgrow.copy(dm,tbxg1,0,tbxg1,0,nc);
+
+    const Box& cbxg1 = amrex::grow(cbx,1);
+
+    for (FArrayBox* cfp : cfp_fabs)
+    {
+        const Box& wbx = cbxg1 & cfp->box();
+        if (wbx.ok())
+        {
+            cvol.resize(wbx);
+            amrex_eb_flux_reg_fineadd_dm(BL_TO_FORTRAN_BOX(wbx),
+                                         BL_TO_FORTRAN_ANYD(*cfp),
+                                         BL_TO_FORTRAN_ANYD(dmgrow),
+                                         BL_TO_FORTRAN_ANYD(cvol),
+                                         BL_TO_FORTRAN_ANYD(volfrac),
+                                         dx, &nc, m_ratio.getVect());
+        }
+    }
+}
+
+
+void
+EBFluxRegister::FineAdd (const MFIter& mfi,
+                         const std::array<FArrayBox const*, AMREX_SPACEDIM>& flux,
+                         const Real* dx, Real dt,
+                         const FArrayBox& volfrac,
+                         const FArrayBox& dm)
+{
+    BL_ASSERT(m_cfpatch.nComp() == flux[0]->nComp());
+
+    const int li = mfi.LocalIndex();
+    Array<FArrayBox*>& cfp_fabs = m_cfp_fab[li];
+    if (cfp_fabs.empty()) return;
+
+    const Box& tbx = mfi.tilebox();
+
+    BL_ASSERT(tbx.cellCentered());
+    AMREX_ALWAYS_ASSERT(tbx.coarsenable(m_ratio));
+    const Box& cbx = amrex::coarsen(tbx, m_ratio);
+    const int nc = m_cfpatch.nComp();
+
+    FArrayBox cvol;
+
+    for (int idim=0; idim < AMREX_SPACEDIM; ++idim)
+    {
+        const Box& lobx = amrex::adjCellLo(cbx, idim);
+        const Box& hibx = amrex::adjCellHi(cbx, idim);
+        for (FArrayBox* cfp : cfp_fabs)
+        {
+            {
+                const Box& lobx_is = lobx & cfp->box();
+                const int side = 0;
+                if (lobx_is.ok())
+                {
+                    cvol.resize(lobx_is);
+                    amrex_eb_flux_reg_fineadd_v(BL_TO_FORTRAN_BOX(lobx_is),
+                                                BL_TO_FORTRAN_ANYD(*cfp),
+                                                BL_TO_FORTRAN_ANYD(*flux[idim]),
+                                                BL_TO_FORTRAN_ANYD(cvol),
+                                                BL_TO_FORTRAN_ANYD(volfrac),
+                                                dx, &dt, &nc, &idim, &side,
+                                                m_ratio.getVect());
+                }
+            }
+            {
+                const Box& hibx_is = hibx & cfp->box();
+                const int side = 1;
+                if (hibx_is.ok())
+                {
+                    cvol.resize(hibx_is);
+                    amrex_eb_flux_reg_fineadd_v(BL_TO_FORTRAN_BOX(hibx_is),
+                                                BL_TO_FORTRAN_ANYD(*cfp),
+                                                BL_TO_FORTRAN_ANYD(*flux[idim]),
+                                                BL_TO_FORTRAN_ANYD(cvol),
+                                                BL_TO_FORTRAN_ANYD(volfrac),
+                                                dx, &dt, &nc, &idim, &side,
+                                                m_ratio.getVect());
                 }
             }
         }
--- Src/EB/AMReX_EBFluxRegister_2d.F90
+++ Src/EB/AMReX_EBFluxRegister_2d.F90
@@ -7,8 +7,9 @@ module amrex_eb_flux_reg_2d_module
   implicit none
   private
 
-  public :: amrex_eb_flux_reg_crseadd_va, amrex_eb_flux_reg_fineadd_va, &
-       amrex_eb_flux_reg_fineadd_dm, amrex_eb_rereflux_from_crse, amrex_eb_rereflux_to_fine
+  public :: amrex_eb_flux_reg_crseadd_va, amrex_eb_flux_reg_crseadd_v, amrex_eb_flux_reg_fineadd_va, &
+       amrex_eb_flux_reg_fineadd_v, amrex_eb_flux_reg_fineadd_dm, amrex_eb_rereflux_from_crse, &
+       amrex_eb_rereflux_to_fine
 
 contains
 
@@ -58,6 +59,52 @@ contains
     end do
 
   end subroutine amrex_eb_flux_reg_crseadd_va
+
+
+  subroutine amrex_eb_flux_reg_crseadd_v (lo, hi, d, dlo, dhi, flag, fglo, fghi, &
+       fx, fxlo, fxhi, fy, fylo, fyhi, &
+       vfrac, vlo, vhi, &
+       dx, dt, nc) &
+       bind(c,name='amrex_eb_flux_reg_crseadd_va')
+    integer, dimension(2), intent(in) :: lo, hi, dlo, dhi, fglo, fghi, fxlo, fxhi, fylo, fyhi, &
+         vlo, vhi
+    integer, intent(in) :: nc
+    integer, intent(in) :: flag(fglo(1):fghi(1),fglo(2):fghi(2))
+    real(rt), intent(in   ) :: fx   (fxlo(1):fxhi(1),fxlo(2):fxhi(2),nc)
+    real(rt), intent(in   ) :: fy   (fylo(1):fyhi(1),fylo(2):fyhi(2),nc)
+    real(rt), intent(in   ) :: vfrac( vlo(1): vhi(1), vlo(2): vhi(2))
+    real(rt), intent(inout) :: d ( dlo(1): dhi(1), dlo(2): dhi(2),nc)
+    real(rt), intent(in) :: dx(2), dt
+
+    integer :: i,j
+    real(rt) :: dtdx, dtdy, volinv
+
+    dtdx = dt/dx(1)
+    dtdy = dt/dx(2)
+
+    do    j = lo(2), hi(2)
+       do i = lo(1), hi(1)
+          if (flag(i,j).eq.crse_fine_boundary_cell .and. vfrac(i,j).gt.1.d-14) then
+             
+             volinv = 1._rt/vfrac(i,j)
+
+             if (flag(i-1,j).eq.fine_cell) then
+                d(i,j,:) = d(i,j,:) - dtdx*fx(i,j,:)*volinv
+             else if (flag(i+1,j).eq.fine_cell) then
+                d(i,j,:) = d(i,j,:) + dtdx*fx(i+1,j,:)*volinv
+             end if
+             
+             if (flag(i,j-1).eq.fine_cell) then
+                d(i,j,:) = d(i,j,:) - dtdy*fy(i,j,:)*volinv
+             else if (flag(i,j+1).eq.fine_cell) then
+                d(i,j,:) = d(i,j,:) + dtdy*fy(i,j+1,:)*volinv
+             end if
+
+          end if
+       end do
+    end do
+
+  end subroutine amrex_eb_flux_reg_crseadd_v
 
 
   subroutine amrex_eb_flux_reg_fineadd_va (lo, hi, d, dlo, dhi, f, flo, fhi, &
@@ -163,6 +210,109 @@ contains
 
     end if
   end subroutine amrex_eb_flux_reg_fineadd_va
+
+
+  subroutine amrex_eb_flux_reg_fineadd_v (lo, hi, d, dlo, dhi, f, flo, fhi, &
+       cvol, clo, chi, vfrac, vlo, vhi, &
+       dx, dt, nc, dir, side, ratio) &
+       bind(c,name='amrex_eb_flux_reg_fineadd_va')
+    integer, dimension(2), intent(in) :: lo, hi, dlo, dhi, flo, fhi, ratio, &
+                  vlo, vhi, clo, chi
+    integer, intent(in) :: nc, dir, side
+    real(rt), intent(in) :: dx(2), dt
+    real(rt), intent(in   ) :: f(flo(1):fhi(1),flo(2):fhi(2),nc)
+    real(rt), intent(inout) :: d(dlo(1):dhi(1),dlo(2):dhi(2),nc)
+    real(rt)                :: cvol ( clo(1): chi(1), clo(2): chi(2))
+    real(rt), intent(in   ) :: vfrac( vlo(1): vhi(1), vlo(2): vhi(2))
+
+    integer :: i,j,n,ii,jj,ioff,joff
+    real(rt) :: fac
+
+    ! dx is fine.  vfrac, ax, ay, and az are fine as well.
+    ! lo and hi are also relative to the fine box.
+
+    do    j = lo(2), hi(2)
+       do i = lo(1), hi(1)
+          cvol(i,j) = sum(vfrac(i*ratio(1):i*ratio(1)+ratio(1)-1,  &
+               &                j*ratio(2):j*ratio(2)+ratio(2)-1))
+          if (cvol(i,j).gt.1.d-14) then
+             cvol(i,j) = 1._rt/cvol(i,j)
+          else
+             cvol(i,j) = 0._rt
+          end if
+       end do
+    end do
+
+    if (dir .eq. 0) then ! x-direction, lo(1) == hi(1)
+
+       fac = dt / dx(1)
+
+       if (side .eq. 0) then ! lo-side
+
+          i = lo(1)
+          ii = (i+1)*ratio(1) !!!
+          
+          do n = 1, nc
+             do j = lo(2), hi(2)
+                do joff = 0, ratio(2)-1
+                   jj = j*ratio(2)+joff
+                   d(i,j,n) = d(i,j,n) - fac*f(ii,jj,n)*cvol(i,j)
+                end do
+             end do
+          end do
+
+       else ! hi-side
+
+          i = lo(1)
+          ii = i*ratio(1) !!!
+
+          do n = 1, nc
+             do j = lo(2), hi(2)
+                do joff = 0, ratio(2)-1
+                   jj = j*ratio(2)+joff
+                   d(i,j,n) = d(i,j,n) + fac*f(ii,jj,n)*cvol(i,j)
+                end do
+             end do
+          end do
+
+       end if
+
+    else  ! y-direction, lo(2) == hi(2)
+
+       fac = dt / dx(2)
+
+       if (side .eq. 0) then ! lo-side
+
+          j = lo(2)
+          jj = (j+1)*ratio(2) !!!
+
+          do n = 1, nc
+             do ioff = 0, ratio(1)-1
+                do i = lo(1), hi(1)
+                   ii = i*ratio(1)+ioff
+                   d(i,j,n) = d(i,j,n) - fac*f(ii,jj,n)*cvol(i,j)
+                end do
+             end do
+          end do
+             
+       else ! hi-side
+
+          j = lo(2)
+          jj = j*ratio(2) !!!
+
+          do n = 1, nc
+             do ioff = 0, ratio(1)-1
+                do i = lo(1), hi(1)
+                   ii = i*ratio(1)+ioff
+                   d(i,j,n) = d(i,j,n) + fac*f(ii,jj,n)*cvol(i,j)
+                end do
+             end do
+          end do
+
+       end if
+
+    end if
+  end subroutine amrex_eb_flux_reg_fineadd_v
 
 
   subroutine amrex_eb_flux_reg_fineadd_dm (lo, hi, d, dlo, dhi, dm, mlo, mhi, &
