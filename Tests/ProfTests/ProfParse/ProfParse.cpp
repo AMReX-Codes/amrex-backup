b5d3d7a601baaeee24fed1cba0750c7af81109b4
Kevin Gott <kngott@lbl.gov>
2018-03-29 13:04:46 -0700

    Updated Flex/Bison parsing test.

--- Tests/ProfTests/ProfParse/ProfParse.cpp
+++ Tests/ProfTests/ProfParse/ProfParse.cpp
@@ -24,23 +24,28 @@ int main(int argc, char *argv[]) {
     RegionsProfStats regProfStats_H;
 
     filename = "bl_prof_H";
-    std::cout << "RPSuccess = " << amrex::ParseFile(filename, &blProfStats_H) << std::endl;
-    std::cout << " ------------ " << std::endl;
-
-    filename = "bl_prof_H";
     std::cout << "RPSuccess = " << amrex::BcastAndParseFile(filename, &blProfStats_H) << std::endl;
     std::cout << " ------------ " << std::endl;
 
+    filename = "bl_call_stats_H";
+    std::cout << "RPSuccess = " << amrex::BcastAndParseFile(filename, &blProfStats_H) << std::endl;
+    std::cout << " ------------ " << std::endl;
+
+    filename = "bl_comm_prof_H";
+    std::cout << "RPSuccess = " << amrex::ParseFile(filename, &blProfStats_H) << std::endl;
+    std::cout << " ------------ " << std::endl;
+
     filename = "bl_prof_H";
     std::cout << "RPSuccess = " << amrex::ParseFile(filename, &blProfStats_H) << std::endl;
     std::cout << " ------------ " << std::endl;
 
+/*
     filename = "bl_prof_D";
     RPSuccess = amrex::BcastAndParseFile(filename, &blProfStats_H);
     std::cout << "RPSuccess = " << RPSuccess << std::endl;
     std::cout << "NProcs = " << blProfStats_H.GetNProcs() << std::endl;
     std::cout << " ------------ " << std::endl;
-
+*/
     filename = "bl_prof_H";
     RPSuccess = amrex::BcastAndParseFile(filename, &blProfStats_H);
     std::cout << "RPSuccess = " << RPSuccess << std::endl;
