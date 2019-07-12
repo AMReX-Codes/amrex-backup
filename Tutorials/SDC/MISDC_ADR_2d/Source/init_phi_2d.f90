4413c6bd3cfa53291247a8e4f48dfc51da0765ba
Kerrek Stinson <kerrekstinson@Kerreks-MBP.dhcp.lbnl.us>
2019-07-12 18:19:06 -0400

    init_phi

--- Tutorials/SDC/MISDC_ADR_2d/Source/init_phi_2d.f90
+++ Tutorials/SDC/MISDC_ADR_2d/Source/init_phi_2d.f90
@@ -11,18 +11,20 @@ subroutine init_phi(lo, hi, phi, philo, phihi, dx, prob_lo, prob_hi) bind(C, nam
   real(amrex_real), intent(in   ) :: prob_hi(2) 
 
   integer          :: i,j
-  double precision :: x,y,tupi
-  tupi=3.14159265358979323846d0*2d0
+  double precision :: x,y,tupi,t0,d
 
-  do j = lo(2), hi(2)
-     y = prob_lo(2) + (dble(j)+0.5d0) * dx(2)
-     do i = lo(1), hi(1)
-        x = prob_lo(1) + (dble(i)) * dx(1)
+  tupi=3.14159265358979323846d0*2d0
+  d=0.1
+  t0=0.0025d0/d  
+  do j = philo(2), phihi(2)
+     y = prob_lo(2) + dble(j) * dx(2)
+     do i = philo(1), phihi(1)
+        x = prob_lo(1) + dble(i) * dx(1)
 
         phi(i,j) =sin(x*tupi)*sin(y*tupi)
+        phi(i,j)=exp(-x*x/(4.0d0*d*t0))*exp(-y*y/(4.0d0*d*t0))        
      end do
   end do
-
 end subroutine init_phi
 
 subroutine err_phi(lo, hi, phi, philo, phihi, dx, prob_lo, prob_hi,a,d,r,time) bind(C, name="err_phi")
@@ -40,25 +42,37 @@ subroutine err_phi(lo, hi, phi, philo, phihi, dx, prob_lo, prob_hi,a,d,r,time) b
   real(amrex_real), intent(in   ) :: a,d,r
   real(amrex_real), intent(in   ) :: time
 
-  integer          :: i,j
-  double precision :: x,y,sym,tupi, maxphi
+  integer          :: i,j,kx,ky,nbox
+  double precision :: x,y,sym,tupi, maxphi,xx,yy,t0
 
   tupi=3.14159265358979323846d0*2d0
 
   !  Form the diffusion coefficient for the 2nd order Laplacian produces
-  sym=d*(-2.0d0+2.0d0*cos(tupi*dx(1)))/(dx(1)*dx(1))
-  sym=sym+d*(-2.0d0+2.0d0*cos(tupi*dx(2)))/(dx(2)*dx(2))
+!  sym=d*(-2.0d0+2.0d0*cos(tupi*dx(1)))/(dx(1)*dx(1))
+!  sym=sym+d*(-2.0d0+2.0d0*cos(tupi*dx(2)))/(dx(2)*dx(2))
+  sym=    d*(-3.0d1+32.0d0*cos(tupi*dx(1))-2.0d0*cos(tupi*2.0d0*dx(1)))/(1.2d1*dx(1)*dx(1))
+  sym=sym+d*(-3.0d1+32.0d0*cos(tupi*dx(2))-2.0d0*cos(tupi*2.0d0*dx(2)) )/(1.2d1*dx(2)*dx(2))
   sym=sym-r    !  Add reaction
 
-  maxphi=-1.0
-  do j = lo(2), hi(2)
-     y = prob_lo(2) + (dble(j)+0.5d0) * dx(2) +a*time
-     do i = lo(1), hi(1)
+  t0=0.0025d0/d
+  nbox = ceiling(sqrt(4.0*d*(t0+time)*37.0)/2.0)  !  Decide how many periodic images
+  
+  do j = philo(2), phihi(2)
+     y = prob_lo(2) + dble(j) * dx(2) +a*time
+     do i = philo(1), phihi(1)
         x = prob_lo(1) + (dble(i)) * dx(1) + a*time
-        phi(i,j) = phi(i,j)-sin(x*tupi)*sin(y*tupi)*exp(time*sym)
-        if (abs(phi(i,j)) .gt. maxphi) maxphi=abs(phi(i,j))
+        !        phi(i,j) = phi(i,j)-sin(x*tupi)*sin(y*tupi)*exp(time*sym)
+
+        do kx = -nbox,nbox
+        do ky = -nbox,nbox
+          xx = x + real(2*kx)
+          yy = y + real(2*ky)
+          phi(i,j) = phi(i,j) - t0/(t0+time)*exp(-xx*xx/(4.0d0*d*(t0+time)))*exp(-yy*yy/(4.0d0*d*(t0+time)))
+       end do
+       end do
+
+        
      end do
   end do
-
-  print *,'max error in phi=',maxphi
+  
 end subroutine err_phi
