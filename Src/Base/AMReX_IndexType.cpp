2ad05a9644f6537cefe522636583187db37f3e4e
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-08-16 07:08:18 -0700

    6d WIP: IndexType

--- Src/Base/AMReX_IndexType.cpp
+++ Src/Base/AMReX_IndexType.cpp
@@ -11,9 +11,12 @@ operator<< (std::ostream&    os,
             const IndexType& it)
 {
     os << '('
-       << AMREX_D_TERM( (it.test(0)?'N':'C'),
-                  << ',' << (it.test(1)?'N':'C'),
-                  << ',' << (it.test(2)?'N':'C')) << ')' << std::flush;
+       << AMREX_D6_TERM( (it.test(0)?'N':'C'),
+               << ',' << (it.test(1)?'N':'C'),
+               << ',' << (it.test(2)?'N':'C'),
+               << ',' << (it.test(3)?'N':'C'),
+               << ',' << (it.test(4)?'N':'C'),
+               << ',' << (it.test(5)?'N':'C')) << ')' << std::flush;
 
     if (os.fail())
         amrex::Error("operator<<(ostream&,IndexType&) failed");
@@ -30,16 +33,22 @@ std::istream&
 operator>> (std::istream& is,
             IndexType&    it)
 {
-    char AMREX_D_DECL(t0,t1,t2);
+    char AMREX_D6_DECL(t0,t1,t2,t3,t4,t5);
 
-    AMREX_D_EXPR( is.ignore(BL_IGNORE_MAX, '(') >> t0,
-            is.ignore(BL_IGNORE_MAX, ',') >> t1,
-            is.ignore(BL_IGNORE_MAX, ',') >> t2);
+    AMREX_D6_EXPR( is.ignore(BL_IGNORE_MAX, '(') >> t0,
+                   is.ignore(BL_IGNORE_MAX, ',') >> t1,
+                   is.ignore(BL_IGNORE_MAX, ',') >> t2,
+                   is.ignore(BL_IGNORE_MAX, ',') >> t3,
+                   is.ignore(BL_IGNORE_MAX, ',') >> t4,
+                   is.ignore(BL_IGNORE_MAX, ',') >> t5 );
     is.ignore(BL_IGNORE_MAX, ')');
     AMREX_D_TERM(
         BL_ASSERT(t0 == 'C' || t0 == 'N'); t0=='N'?it.set(0):it.unset(0); ,
         BL_ASSERT(t1 == 'C' || t1 == 'N'); t1=='N'?it.set(1):it.unset(1); ,
-        BL_ASSERT(t2 == 'C' || t2 == 'N'); t2=='N'?it.set(2):it.unset(2));
+        BL_ASSERT(t2 == 'C' || t2 == 'N'); t2=='N'?it.set(2):it.unset(2); ,
+        BL_ASSERT(t3 == 'C' || t3 == 'N'); t3=='N'?it.set(3):it.unset(3); ,
+        BL_ASSERT(t4 == 'C' || t4 == 'N'); t4=='N'?it.set(4):it.unset(4); ,
+        BL_ASSERT(t5 == 'C' || t5 == 'N'); t5=='N'?it.set(5):it.unset(5); );
 
     if (is.fail())
         amrex::Error("operator>>(ostream&,IndexType&) failed");
